d50a59d8367a3451a38b3b828dbccee8321ceb60
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-02-23 19:07:55 -0700

    added container div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Weather from "./Weather";
+
 import "./Weather.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,9 @@
 import React from "react";
 //import axios from "axios";
 
+import "./Weather.css";
+import "./App.css";
+
 export default function Weather() {
   let weatherData = {
     city: "Phoenix, AZ",
@@ -12,98 +15,100 @@ export default function Weather() {
     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
   };
   return (
-    <div className="weather">
-      <div className="search">
-        <form className="row">
-          <div className="col-6">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search a City"
-              autocomplete="off"
-              autofocus="on"
-            />
-          </div>
-          <div className="col">
-            <button
-              type="submit"
-              className="btn btn-primary mb-3"
-              value="search"
-            >
-              Submit
-            </button>
-
-            <span>
-              <button className="btn btn-success mb-3 current-location">
-                Current Location
+    <div className="container">
+      <div className="weather">
+        <div className="search">
+          <form className="row">
+            <div className="col-6">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search a City"
+                autocomplete="off"
+                autofocus="on"
+              />
+            </div>
+            <div className="col">
+              <button
+                type="submit"
+                className="btn btn-primary mb-3"
+                value="search"
+              >
+                Submit
               </button>
-            </span>
-          </div>
-        </form>
-      </div>
-      <br />
-      <div className="row">
-        <div className="col-6 float-left" styles="width: 200px">
-          <div className="card" styles="width: 15rem">
-            <div className="card-body">
-              <div className="float-left weather-temperature">
-                <img
-                  src={weatherData.imgUrl}
-                  className="weather-icon float-left"
-                  alt="weather-icon"
-                />
-                <strong className="temperature">
-                  {weatherData.temperature}{" "}
-                </strong>
-                <span className="units">
-                  <a href="/" className="active">
-                    °C{""}
-                  </a>
-                  |
-                  <a href="/" className="fahrenheit-sign">
-                    °F
-                  </a>
-                </span>
-              </div>
-              <div className="weather-attributes">
-                <h6 className="card-subtitle mb-2 text-muted weather-condition">
-                  {" "}
-                </h6>
-                <br />
 
-                <h6 className="card-subtitle mb-2 text-muted">
-                  Humidity: {weatherData.humidity}
-                </h6>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  Wind: {weatherData.wind}
-                </h6>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  Air Quality: {weatherData.air}
-                </h6>
+              <span>
+                <button className="btn btn-success mb-3 current-location">
+                  Current Location
+                </button>
+              </span>
+            </div>
+          </form>
+        </div>
+        <br />
+        <div className="row">
+          <div className="col-6 float-left" styles="width: 200px">
+            <div className="card" styles="width: 15rem">
+              <div className="card-body">
+                <div className="float-left weather-temperature">
+                  <img
+                    src={weatherData.imgUrl}
+                    className="weather-icon float-left"
+                    alt="weather-icon"
+                  />
+                  <strong className="temperature">
+                    {weatherData.temperature}{" "}
+                  </strong>
+                  <span className="units">
+                    <a href="/" className="active">
+                      °C{""}
+                    </a>
+                    |
+                    <a href="/" className="fahrenheit-sign">
+                      °F
+                    </a>
+                  </span>
+                </div>
+                <div className="weather-attributes">
+                  <h6 className="card-subtitle mb-2 text-muted weather-condition">
+                    {" "}
+                  </h6>
+                  <br />
+
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    Humidity: {weatherData.humidity}
+                  </h6>
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    Wind: {weatherData.wind}
+                  </h6>
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    Air Quality: {weatherData.air}
+                  </h6>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="col-6 mt-5" styles="width: 200px">
-          <div class="current-city">
-            <h2>{weatherData.city}</h2>
-          </div>
+          <div class="col-6 mt-5" styles="width: 200px">
+            <div class="current-city">
+              <h2>{weatherData.city}</h2>
+            </div>
 
-          <span class="date">
-            <h5>{weatherData.date}</h5>
-          </span>
+            <span class="date">
+              <h5>{weatherData.date}</h5>
+            </span>
+          </div>
         </div>
+        <br />
+        <div class="weather-forecast"></div>
+        <a
+          href="https://github.com/ytisdel/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-Sourced
+        </a>{" "}
+        by Yalona Tisdel
       </div>
-      <br />
-      <div class="weather-forecast"></div>
-      <a
-        href="https://github.com/ytisdel/weather-react-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Open-Sourced
-      </a>{" "}
-      by Yalona Tisdel
     </div>
   );
 }
